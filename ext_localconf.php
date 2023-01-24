46238fdcc8cbcd5dd4a6b5c59ef7737176eda4ad
Thomas Schöne <thomas.schoene@netresearch.de>
2023-01-24 15:41:23 +0100

    NRLF-295: Make api credentials configurable

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,3 +21,13 @@ if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cowriter'])) {
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cowriter'] = 'EXT:t3_cowriter/Configuration/RTE/Plugin.yaml';
 }
 
+
+// Make the extension configuration accessible
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+);
+$config = $extensionConfiguration->get('t3_cowriter');
+
+$renderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+$renderer->addJsInlineCode('cowriterkey', 'const OPENAI_KEY = "' . $config['openaiKey'] . '"', false, true);
+$renderer->addJsInlineCode('cowriterorg', 'const OPENAI_ORG = "' . $config['openaiOrg'] . '"', false, true);
