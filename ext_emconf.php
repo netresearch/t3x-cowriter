f8d86267a1e84eca0eaddfdd2c138dbfca6e8059
Martin Wunderlich <martin.wunderlich@netresearch.de>
2024-01-02 08:25:38 +0100

    NEXT-21: Add new constraints for TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,12 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Netresearch DTT GmbH',
     'author_email' => 'info@netresearch.de',
     'clearCacheOnLoad' => 0,
-    'version' => '1.2.5',
+    'version' => '2.0.0',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
-            'rte_ckeditor' => '9.5.0-11.5.99',
+            'typo3' => '12.0.0-12.5.99',
+            'rte_ckeditor' => '12.0.0-12.5.99',
         ],
         'conflicts' => [],
         'suggests' => []
