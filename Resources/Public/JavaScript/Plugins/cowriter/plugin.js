fc809bf12dc444d700e36cbbfe6c5252aac50933
Martin Wunderlich <martin.wunderlich@netresearch.de>
2024-01-04 09:23:36 +0000

    NEXT-21: Add exemplaric plugin skeleton

--- Resources/Public/JavaScript/Plugins/cowriter/plugin.js
+++ Resources/Public/JavaScript/Plugins/cowriter/plugin.js
@@ -1,172 +1,56 @@
-CKEDITOR.dialog.add("cowriterDialog", function(editor) {
-  let select_model = "gpt-3.5-turbo-instruct", select_temperature = 0.5, select_max_tokens = 4e3, select_amount = 1;
-  const escapeHtml = (unsafe) => {
-    return unsafe.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll('"', "&quot;").replaceAll("'", "&#039;");
-  };
-  return {
-    title: "Cowriter",
-    minWidth: 400,
-    minHeight: 70,
-    contents: [
-      {
-        id: "tab-basic",
-        label: editor.lang.cowriter.tabGeneral,
-        accessKey: "C",
-        elements: [
-          {
-            type: "textarea",
-            id: "cowriter",
-            label: editor.lang.cowriter.writeAbout,
-            rows: 6,
-            validate: CKEDITOR.dialog.validate.notEmpty(editor.lang.cowriter.errorNotEmpty),
-            setup: function(element) {
-              this.setValue(element.getText());
-            },
-            commit: function(element) {
-              element.setText(" Loading … ");
-              var xhr = new XMLHttpRequest();
-              xhr.open("POST", "https://api.openai.com/v1/completions", true);
-              xhr.setRequestHeader("Content-Type", "application/json");
-              xhr.setRequestHeader("Authorization", "Bearer " + OPENAI_KEY);
-              xhr.setRequestHeader("OpenAI-Organization", OPENAI_ORG);
-              xhr.send(JSON.stringify({
-                prompt: this.getValue(),
-                // Text to complete
-                max_tokens: select_max_tokens,
-                // 1 to 4000
-                model: select_model,
-                // 'gpt-3.5-turbo-instruct', 'gpt-3.5-turbo'
-                temperature: select_temperature,
-                // 0.0 is equivalent to greedy sampling
-                top_p: 1,
-                // 1.0 is equivalent to greedy sampling
-                n: select_amount,
-                // Number of results to return
-                frequency_penalty: 0,
-                // 0.0 is equivalent to no penalty
-                presence_penalty: 0
-                // 0.0 is equivalent to no penalty
-              }));
-              xhr.onreadystatechange = function() {
-                if (this.readyState === 4) {
-                  if (this.status === 200) {
-                    let completeText = "", choices = JSON.parse(this.responseText).choices;
-                    for (let i = 0; i < choices.length; i++) {
-                      completeText += "<p>" + escapeHtml(choices[i].text) + "</p>";
-                    }
-                    element.setHtml(completeText);
-                  } else {
-                    element.setText(" Error: " + this.responseText);
-                  }
-                }
-              };
-              xhr.onerror = function() {
-                element.setText(" Error: " + this.responseText);
-              };
-            }
-          }
-        ]
-      },
-      {
-        id: "tab-advanced",
-        label: editor.lang.cowriter.tabAdvanced,
-        elements: [
-          // Add select field with options to choose the model from openai api.
-          {
-            type: "select",
-            id: "model",
-            title: editor.lang.cowriter.modelSelction,
-            label: editor.lang.cowriter.modelSelctionLabel,
-            default: "gpt-3.5-turbo-instruct",
-            items: [
-              ["3.5 Turbo Instruct", "gpt-3.5-turbo-instruct"],
-              ["3.5 Turbo", "gpt-3.5-turbo"]
-            ],
-            setup: function(element) {
-              this.setValue(element.getText());
-            }
-          },
-          // Add select field with different temperatures from 0 to 2
-          {
-            type: "select",
-            id: "temperature",
-            title: editor.lang.cowriter.temperature,
-            label: editor.lang.cowriter.temperatureLabel,
-            default: 0.5,
-            items: [
-              ["0.0", 0.01],
-              ["0.25", 0.25],
-              ["0.5", 0.5],
-              ["0.75", 0.75],
-              ["1.0", 1],
-              ["1.25", 1.25],
-              ["1.5", 1.5],
-              ["1.75", 1.75],
-              ["2.0", 2]
-            ],
-            setup: function(element) {
-              element.setAttribute("type", "number");
-              this.setValue(element.getText());
-            },
-            commit: function(element) {
-            }
-          },
-          // Add select field for number of results
-          {
-            type: "select",
-            id: "amount",
-            title: editor.lang.cowriter.amount,
-            label: editor.lang.cowriter.amountLabel,
-            default: 1,
-            items: [
-              ["1", 1],
-              ["2", 2],
-              ["3", 3],
-              ["4", 4]
-            ],
-            setup: function(element) {
-              element.setAttribute("type", "number");
-              this.setValue(element.getText());
-            },
-            commit: function(element) {
-            }
-          }
-        ]
-      }
-    ],
-    onOk: function() {
-      let dialog = this, cowriter = editor.document.createElement("div");
-      select_model = dialog.getValueOf("tab-advanced", "model");
-      select_temperature = parseFloat(dialog.getValueOf("tab-advanced", "temperature"));
-      select_amount = parseInt(dialog.getValueOf("tab-advanced", "amount"));
-      switch (select_model) {
-        case "gpt-3.5-turbo-instruct":
-          select_max_tokens = 4e3;
-          break;
-        case "gpt-3.5-turbo":
-          select_max_tokens = 2e3;
-          break;
-        default:
-          select_max_tokens = 4e3;
-      }
-      dialog.commitContent(cowriter);
-      editor.insertElement(cowriter);
+import {Core, UI} from "@typo3/ckeditor5-bundle.js";
+
+export default class Cowriter extends Core.Plugin {
+    static pluginName = 'cowriter';
+
+    init() {
+        const editor = this.editor;
+        const model = editor.model;
+        const view = editor.view;
+
+        // Register <wbr>:
+        model.schema.register('wbrTag', {
+            allowWhere: '$text',
+            isInline: true
+        });
+
+        editor.conversion.for('upcast')
+            .elementToElement({
+                model: 'wbrTag',
+                view: 'wbr'
+            });
+
+        editor.conversion.for('dataDowncast')
+            .elementToElement({
+                model: 'wbrTag',
+                view: (modelElement, {writer}) => writer.createEmptyElement('wbr')
+            });
+
+        // Wrap <wbr> with <span> element to highlight the element in the editing view:
+        editor.conversion.for('editingDowncast')
+            .elementToElement({
+                model: 'wbrTag',
+                view: (modelElement, {writer}) => writer.createContainerElement('span', {class: 'ck ck-wbr'}, writer.createEmptyElement('wbr'))
+            });
+
+        // Create button that allows to insert <wbr> at current text cursor position:
+        editor.ui.componentFactory.add('Cowriter', () => {
+            const button = new UI.ButtonView();
+
+            button.set({
+                label: 'Cowriter',
+                icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" xml:space="preserve"><path d="M6,4 L6,5 L4,5 L4,6 L6,6 L6,7 L4,7 L4,8 L6,8 L6,9 L3,9 L3,4 L6,4 Z M7,7 L7,8 L6,8 L6,7 L7,7 Z M7,5 L7,6 L6,6 L6,5 L7,5 Z M11,4 L11,5 L9,5 L9,6 L11,6 L11,7 L12,7 L12,9 L11,9 L11,8 L10,8 L10,7 L9,7 L9,9 L8,9 L8,4 L11,4 Z M12,5 L12,6 L11,6 L11,5 L12,5 Z M14,8 L16,8 L16,9 L13,9 L13,4 L16,4 L16,5 L14,5 L14,6 L15,6 L15,7 L14,7 L14,8 Z M6,11 L6,16 L5,16 L5,14 L4,14 L4,16 L3,16 L3,11 L6,11 Z M5,13 L5,12 L4,12 L4,13 L5,13 Z M8,11 L8,13 L9,13 L9,14 L8,14 L8,16 L7,16 L7,11 L8,11 Z M11,15 L11,16 L10,16 L10,15 L11,15 Z M14,11 L14,13 L16,13 L16,11 L17,11 L17,14 L14,14 L14,16 L13,16 L13,15 L12,15 L12,14 L11,14 L11,13 L12,13 L12,12 L13,12 L13,11 L14,11 Z M10,14 L10,15 L9,15 L9,14 L10,14 Z M10,12 L10,13 L9,13 L9,12 L10,12 Z M11,11 L11,12 L10,12 L10,11 L11,11 Z"/></svg>'
+            });
+
+            button.on('execute', () => {
+                model.change(writer => {
+                    const insertPosition = editor.model.document.selection.getFirstPosition();
+                    const wbrTag = writer.createElement('wbrTag');
+                    writer.insert(wbrTag, insertPosition);
+                });
+            });
+
+            return button;
+        });
     }
-  };
-});
-CKEDITOR.plugins.add("cowriter", {
-  icons: "cowriter",
-  lang: ["en", "de"],
-  init: function(editor) {
-    editor.addCommand("cowriter", new CKEDITOR.dialogCommand("cowriterDialog"));
-    editor.ui.addButton("Cowriter", {
-      label: "Co-Writer",
-      command: "cowriter",
-      toolbar: "insert",
-      icon: this.path + "icons/cowriter-logo.png"
-    });
-  }
-});
-CKEDITOR.config.keystrokes = [
-  [CKEDITOR.ALT + 67, "cowriter"]
-];
+}
