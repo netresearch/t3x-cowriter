e3b4eeb9931333519dd4e092e4d7386e34b66fcc
DevMiner <devminer@devminer.xyz>
2024-02-16 13:15:50 +0000

    NEXT-21: Fix typos

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 't3_cowriter',
-    'description' => 'With the help of ai you can now work on a page together with a cowriter - a digital assistant that helps you to write your content.',
+    'description' => 'With the help of AI you can now work on a page together with a cowriter - a digital assistant that helps you to write your content.',
     'category' => 'misc',
     'author' => 'Netresearch DTT GmbH',
     'author_email' => 'info@netresearch.de',
