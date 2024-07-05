6676085be6cec024bd0e0f845dec19192a61afd2
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-05 10:51:40 +0000

    NEXT-56: Domain Model definition

---
+++ Classes/Domain/Model/prompts.php
@@ -0,0 +1,34 @@
+<?php
+namespace netresearch\t3_cowriter\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+class prompts extends AbstractEntity {
+    /**
+     * @var string
+     */
+    protected $title = '';
+
+    /**
+     * @var string
+     */
+    protected $prompt = '';
+
+    // Getter und Setter Methoden
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getPrompt() {
+        return $this->prompt;
+    }
+
+    public function setPrompt($prompt) {
+        $this->prompt = $prompt;
+    }
+}
