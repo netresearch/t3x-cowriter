8394b3d429060e817c2ad41e10223377ca0fd53b
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-08 09:16:13 +0000

    NEXT-56: create Domain Model

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,10 +1,4 @@
-CREATE TABLE tx_t3cowriter_domain_model_prompt (
+CREATE TABLE tx_t3cowriter_domain_model_prompts (
     title varchar(255) DEFAULT '' NOT NULL,
     prompt text DEFAULT '' NOT NULL
-);
-
-CREATE TABLE tx_t3cowriter_domain_model_contentelement (
-    title varchar(255) DEFAULT '' NOT NULL,
-    table varchar(255) DEFAULT '' NOT NULL,
-    field text DEFAULT '' NOT NULL
 );
