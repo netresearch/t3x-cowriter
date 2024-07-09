1d34d56bcddfc6503e1730113ebaed85c436ffcf
Thomas Schöne <thomas.schoene@netresearch.de>
2024-07-09 08:19:26 +0000

    NEXT-64: WIP

---
+++ Classes/Domain/Repository/ContentElementRepository.php
@@ -0,0 +1,9 @@
+<?php
+namespace Netresearch\T3Cowriter\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class ContentElementRepository extends Repository
+{
+    // Zusätzliche Methoden falls erforderlich
+}
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,3 +2,8 @@ CREATE TABLE tx_t3cowriter_domain_model_prompt (
     title varchar(255) DEFAULT '' NOT NULL,
     prompt text DEFAULT '' NOT NULL
 );
+
+CREATE TABLE tx_t3cowriter_domain_model_contentelement (
+    table varchar(255) DEFAULT '' NOT NULL,
+    field text DEFAULT '' NOT NULL
+);
