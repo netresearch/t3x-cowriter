5e6af1c48174d99847033ac61745fdcbfb5b15b8
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-11 06:18:04 +0000

    NEXT-56: Change comments

--- Classes/Domain/Repository/PromptsRepository.php
+++ Classes/Domain/Repository/PromptsRepository.php
@@ -3,7 +3,13 @@ namespace netresearch\t3_cowriter\Domain\Repository;
 
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
-// Definition of the PromptsRepository class that extends the Repository class
+/**
+ * Definition of the PromptsRepository class that extends the Repository class
+ *
+ * @package Netresearch\SuperAdmin
+ * @author  Philipp Altmann <philipp.altmann@netresearch.de>
+ * @license https://www.gnu.org/licenses/gpl-3.0.de.html GPL-3.0-or-later
+ */
 class PromptsRepository extends Repository
 {
     // Additional methods if required
