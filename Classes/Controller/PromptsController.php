7eaa09fdfd363054f02c889398cff766cf1aaea2
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-11 09:09:09 +0000

    NEXT-64: Add comments DocBlock

--- Classes/Controller/PromptsController.php
+++ Classes/Controller/PromptsController.php
@@ -6,6 +6,15 @@ use Netresearch\T3Cowriter\Domain\Model\Prompts;
 use Netresearch\T3Cowriter\Domain\Repository\PromptsRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
+/**
+ *
+ * Controller for handling Prompts actions
+ *
+ * @package Netresearch\T3Cowriter
+ * @author  Philipp Altmann <philipp.altmann@netresearch.de>
+ * @license https://www.gnu.org/licenses/gpl-3.0.de.html GPL-3.0-or-later
+ *
+ */
 class PromptsController extends ActionController
 {
     /**
@@ -13,17 +22,38 @@ class PromptsController extends ActionController
      */
     protected $promptsRepository = null;
 
+
+    /**
+     *
+     * Injects the PromptsRepository
+     *
+     * @param PromptsRepository $promptsRepository
+     * @return void
+     */
     public function injectPromptsRepository(PromptsRepository $promptsRepository)
     {
         $this->promptsRepository = $promptsRepository;
     }
 
+    /**
+     *
+     * Lists all prompt models
+     *
+     * @return void
+     */
     public function listAction()
     {
         $models = $this->promptsRepository->findAll();
         $this->view->assign('models', $models);
     }
 
+    /**
+     *
+     * Displays a single prompt model
+     *
+     * @param Prompts $model
+     * @return void
+     */
     public function showAction(Prompts $model)
     {
         $this->view->assign('model', $model);
