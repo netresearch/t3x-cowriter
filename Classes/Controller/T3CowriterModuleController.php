de2f677d7b486d4d1b29c792b9ca431ce60b379d
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-16 07:17:28 +0000

    NEXT-57: create BE Module in TYPO3 v12

--- Classes/Controller/T3CowriterModuleController.php
+++ Classes/Controller/T3CowriterModuleController.php
@@ -1,24 +1,26 @@
 <?php
+
 namespace Netresearch\T3Cowriter\Controller;
 
-// the module template will be initialized in handleRequest()
+use TYPO3\CMS\Backend\Attribute\AsController;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
+#[AsController]
 final class T3CowriterModuleController extends ActionController
 {
     public function __construct(
         protected readonly ModuleTemplateFactory $moduleTemplateFactory
-        // ...
     ) {
     }
 
     // use Psr\Http\Message\ResponseInterface
-    public function indexAction(): \TYPO3\CMS\Core\Http\ResponseFactory {
-        $this->view->assign('someVar', 'someContent');
+    public function indexAction(): ResponseInterface {
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
-        $moduleTemplate->setContent($this->view->render());
-        return $this->htmlResponse($moduleTemplate->renderContent());
+        /*
+            $moduleTemplate->assign('aVariable', 'aValue');
+        */
+        return $moduleTemplate->renderResponse('Index');
     }
 }
