a6970455e32432c433b55e8674b822a2cc2f9da8
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-16 08:50:29 +0000

    NEXT-57: Add comments

--- Classes/Controller/T3CowriterModuleController.php
+++ Classes/Controller/T3CowriterModuleController.php
@@ -10,11 +10,21 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 #[AsController]
 final class T3CowriterModuleController extends ActionController
 {
+    /**
+     *  Constructor method for T3CowriterModuleController.
+     * 
+     * @param ModuleTemplateFactory $moduleTemplateFactory
+     */
     public function __construct(
         protected readonly ModuleTemplateFactory $moduleTemplateFactory
     ) {
     }
 
+    /**
+     *  Index action method to render the default view.
+     *
+     * @return ResponseInterface
+     */
     public function indexAction(): ResponseInterface {
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
         return $moduleTemplate->renderResponse('Index');
