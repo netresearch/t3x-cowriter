d8fe701cfde82a9f947e3c827a23c00c37f969f2
Philipp Altmann <philipp.altmann@netresearch.de>
2024-07-16 07:17:28 +0000

    NEXT-57: create BE Module in TYPO3 v12

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,22 +1,22 @@
 <?php
 
-
+use Netresearch\T3Cowriter\Controller\T3CowriterModuleController;
 
 /**
  * Definitions for modules provided by EXT:examples
  */
 return [
-    'T3CowriterModuleController' => [
+    'T3CowriterModule' => [
         'parent' => 'web',
         'position' => ['bottom'],
         'access' => 'admin',
         'workspaces' => 'live',
-        'path' => '/module/page/example',
-        'labels' => 'LLL:EXT:t3_cowriter/Resources/Private/Language/locallang_db.xlf',
+        'path' => '/module/page/t3cowriter',
+        'labels' => 'LLL:EXT:t3_cowriter/Resources/Private/Language/locallang.xlf',
         'extensionName' => 't3-cowriter',
         'controllerActions' => [
             T3CowriterModuleController::class => [
-                'indexAction'
+                'index'
             ]
         ],
     ]
