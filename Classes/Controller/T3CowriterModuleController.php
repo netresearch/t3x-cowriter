e850376729064d0a9a553acfcfddb5b2653b86fe
philipp.altmann <philipp.altmann@netresearch.de>
2024-07-19 11:24:07 +0200

    NEXT-60: Create textfield tables

--- Classes/Controller/T3CowriterModuleController.php
+++ Classes/Controller/T3CowriterModuleController.php
@@ -9,6 +9,8 @@ use ScssPhp\ScssPhp\Formatter\Debug;
 use TYPO3\CMS\Backend\Attribute\AsController;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use Netresearch\T3Cowriter\Domain\Repository\ContentElementRepository;
 use Netresearch\T3Cowriter\Domain\Model\ContentElement;
@@ -59,11 +61,9 @@ class T3CowriterModuleController extends ActionController
      * @return ResponseInterface
      */
     public function indexAction(): ResponseInterface {
-        $contentElements = $this->contentElementRepository->findAll();
-        $prompts = $this->promptRepository->findAll();
-
-        $this->view->assign('contentElements', $contentElements);
-        $this->view->assign('prompts', $prompts);
+        $this->view->assign('contentElements', $this->contentElementRepository->findAll());
+        $this->view->assign('prompts', $this->promptRepository->findAll());
+        $this->view->assign('pages', $this->getAllPagesWithTextFieldsElements());
         return $this->moduleResponse();
     }
 
@@ -76,4 +76,66 @@ class T3CowriterModuleController extends ActionController
         $moduleTemplate->setContent($this->view->render());
         return $this->htmlResponse($moduleTemplate->renderContent());
     }
+
+    /**
+     *
+     *  Retrieves pages with text field elements and organizes the data.
+     *  Joins tt_content and pages tables, filters out empty and newline-only bodytext fields.
+     *
+     * @return array
+     * @throws \Doctrine\DBAL\Exception
+     */
+    private function getAllPagesWithTextFieldsElements() : array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+        $queryBuilder
+            ->select('tt_content.pid', 'pages.title', 'tt_content.CType')
+            ->addSelectLiteral('COUNT(*) AS quantity')
+            ->from('tt_content')
+            ->join(
+                'tt_content',
+                'pages',
+                'pages',
+                $queryBuilder->expr()->eq('tt_content.pid', $queryBuilder->quoteIdentifier('pages.uid'))
+            )
+            ->where(
+                $queryBuilder->expr()->neq('bodytext', $queryBuilder->createNamedParameter('')),
+                $queryBuilder->expr()->isNotNull('bodytext'),
+                $queryBuilder->expr()->neq('bodytext', $queryBuilder->createNamedParameter('\n'))
+            )
+            ->groupBy('tt_content.pid', 'tt_content.CType', 'pages.title');
+
+        $statement = $queryBuilder->executeQuery();
+        $results = $statement->fetchAllAssociative();
+
+        // Organize the data into an array by pid and CType
+        $organizedResults = [];
+        foreach ($results as $row) {
+            $pid = $row['pid'];
+            $CType = $row['CType'];
+            $quantity = $row['quantity'];
+            $title = $row['title'];
+
+            if (!isset($organizedResults[$pid])) {
+                $organizedResults[$pid] = [
+                    'title' => $title,
+                    'CType' => []
+                ];
+            }
+            $organizedResults[$pid]['CType'][$CType] = $quantity;
+        }
+        return $organizedResults;
+    }
 }
+
+/*
+ * SELECT uid, pid, header, bodytext, Ctype, COUNT(*) AS Anzahl
+FROM
+    tt_content
+WHERE
+    bodytext <> ''
+    AND bodytext IS NOT NULL
+    AND HEX(bodytext) <> '0A'
+GROUP BY
+    pid, CType;
+ */
