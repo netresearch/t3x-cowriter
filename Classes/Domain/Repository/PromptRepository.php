8124c7c8b1257867c8e501549355a77013bea1ae
philipp.altmann <philipp.altmann@netresearch.de>
2024-07-22 10:51:42 +0200

    NEXT-56: change prompt name

--- Classes/Domain/Repository/PromptRepository.php
+++ Classes/Domain/Repository/PromptRepository.php
@@ -1,30 +1,16 @@
 <?php
-namespace Netresearch\T3Cowriter\Domain\Repository;
+namespace netresearch\t3_cowriter\Domain\Repository;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\Repository;
-use Netresearch\T3Cowriter\Domain\Model\Prompt;
 
 /**
  * Definition of the PromptsRepository class that extends the Repository class
  *
- * @package Netresearch\T3Cowriter
+ * @package Netresearch\t3_cowriter
  * @author  Philipp Altmann <philipp.altmann@netresearch.de>
  * @license https://www.gnu.org/licenses/gpl-3.0.de.html GPL-3.0-or-later
  */
 class PromptRepository extends Repository
 {
-    public function __construct(
-
-    ) {
-        parent::__construct();
-        /** @var QuerySettingsInterface $querySettings */
-        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
-        // Show comments from all pages
-        $querySettings->setRespectStoragePage(false);
-        $this->setDefaultQuerySettings($querySettings);
-    }
-
+    // Additional methods if required
 }
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tx_t3cowriter_domain_model_prompts (
+CREATE TABLE tx_t3cowriter_domain_model_prompt (
     title varchar(255) DEFAULT '' NOT NULL,
     prompt text DEFAULT '' NOT NULL
 );
