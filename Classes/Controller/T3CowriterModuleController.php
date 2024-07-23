bfb6ec83f09fdccc77db8cd68db1ab382880dbfc
philipp.altmann <philipp.altmann@netresearch.de>
2024-07-23 08:54:03 +0200

    NEXT-58: remove unused use statments and add missing DocBlock

--- Classes/Controller/T3CowriterModuleController.php
+++ Classes/Controller/T3CowriterModuleController.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace Netresearch\T3Cowriter\Controller;
 
 use Netresearch\T3Cowriter\Domain\Repository\PromptRepository;
-use ScssPhp\ScssPhp\Formatter\Debug;
 use TYPO3\CMS\Backend\Attribute\AsController;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use Netresearch\T3Cowriter\Domain\Repository\ContentElementRepository;
 use Netresearch\T3Cowriter\Domain\Model\ContentElement;
-use TYPO3\CMS\Extbase\Persistence\Repository;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
+/**
+ * handles the display and management of content elements and prompts within the T3Cowriter package.
+ * It retrieves data from repositories and uses a template factory to render the module's output.
+ *
+ * @package Netresearch\T3Cowriter
+ * @author  Philipp Altmann <philipp.altmann@netresearch.de>
+ * @license https://www.gnu.org/licenses/gpl-3.0.de.html GPL-3.0-or-later
+ */
 #[AsController]
 class T3CowriterModuleController extends ActionController
 {
