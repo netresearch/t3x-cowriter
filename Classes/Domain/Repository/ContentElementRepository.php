64203526b1bdda1e50d46e3dd8190d5b6970e04c
philipp.altmann <philipp.altmann@netresearch.de>
2024-07-26 09:41:46 +0200

    NEXT-61: Refactor getAllTextFieldElements

--- Classes/Domain/Repository/ContentElementRepository.php
+++ Classes/Domain/Repository/ContentElementRepository.php
@@ -1,6 +1,7 @@
 <?php
 namespace Netresearch\T3Cowriter\Domain\Repository;
 
+use Netresearch\T3Cowriter\Controller\T3CowriterModuleController;
 use phpDocumentor\Reflection\Types\Parent_;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -19,5 +20,79 @@ class ContentElementRepository extends Repository
         // Show comments from all pages
         $querySettings->setRespectStoragePage(false);
         $this->setDefaultQuerySettings($querySettings);
+    }
+
+    /**
+     * Adds where conditions for all fields in the query.
+     *
+     * @param array $fields
+     * @param \TYPO3\CMS\Core\Database\Query\QueryBuilder $queryBuilder
+     * @return void
+     */
+    public function addWhereForAllFields(array $fields, \TYPO3\CMS\Core\Database\Query\QueryBuilder $queryBuilder): void
+    {
+        foreach ($fields as $field) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->neq('table.' . $field, $queryBuilder->createNamedParameter('')),
+                $queryBuilder->expr()->isNotNull('table.' . $field),
+                $queryBuilder->expr()->neq('table.' . $field, $queryBuilder->createNamedParameter('\n'))
+            );
+        };
+    }
+
+    /**
+     * Fetches content elements by their UIDs.
+     *
+     * @param array $selectedContentElements
+     * @param T3CowriterModuleController $t3CowriterModuleController
+     * @return array
+     */
+    public function fetchContentElementsByUid(array $selectedContentElements): array
+    {
+        $contentElements = [];
+        foreach ($selectedContentElements as $uid) {
+            $contentElements[] = $this->findByUid($uid);
+        }
+        return $contentElements;
+    }
+
+    /**
+     * Retrieves all text field elements.
+     *
+     * @param array $contentElements
+     * @param T3CowriterModuleController $t3CowriterModuleController
+     * @return array
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function getAllTextFieldElements(array $contentElements, int $pageId): array
+    {
+        $results = [];
+        foreach ($contentElements as $contentElement) {
+            $fields = explode(',', $contentElement->getField());
+            $tableName = $contentElement->getTable();
+
+            if (!isset($results[$tableName])) {
+                $results[$tableName] = [
+                    'fields' => [],
+                    'elements' => []
+                ];
+            }
+
+            $results[$tableName]['fields'] = array_unique(array_merge($results[$tableName]['fields'], $fields));
+
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($tableName);
+            $queryBuilder
+                ->select('*')
+                ->from($tableName, 'table')
+                ->where(
+                    $queryBuilder->expr()->eq('table.pid', $queryBuilder->createNamedParameter($pageId))
+                );
+            $this->addWhereForAllFields($fields, $queryBuilder);
+            $statement = $queryBuilder->executeQuery();
+            $tableResults = $statement->fetchAllAssociative();
+
+            $results[$tableName]['elements'] = $tableResults;
+        }
+        return $results;
     }
 }
